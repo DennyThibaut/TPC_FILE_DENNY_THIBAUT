c4e181c26ebf741548fd1a728280bf5d8d49cdc1
DESKTOP-DRVIU9M\Utilisateur <t.denny@ludus-academie.com>
2018-02-27 01:47:32 +0100

    no message

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    //type entier :
+    short entierCourt;
+    int entier;
+    long entierLong;
+
+
+    printf("Hello world!\n");
+    system("pause");
+    return 0;
+}
